0a7b18fd6afb5a2f1c4fac7e78fd440b446f42b7
Dilkhush Soni <dilkhush.soni@globallogic.com>
2014-09-26 18:07:13 +0530

    done with some error handling for cities,  and states

--- lib/world_wrap.rb
+++ lib/world_wrap.rb
@@ -1,5 +1,5 @@
 require 'country'
-require 'states'
+require 'state'
 require 'city'
 module WorldWrap
   def countries options = {}
@@ -9,12 +9,21 @@ module WorldWrap
   def states options = {}
     return "Please pass a country name or code" if options[:country].nil? || options[:country].empty?
     country = options[:country].downcase
+    return "not a valid key" if is_invalid_key?(COUNTRIES, country)
+    return "Need to save data for #{country}" unless STATES.has_key?(country)
     STATES[country]
   end
 
   def cities options = {}
     return "Please pass a state name or code" if options[:state].nil? || options[:state].empty?
     state = options[:state].downcase
+#    return "not a valid key" if is_invalid_key?(STATES, state) TODO
+    return "Need to store data for #{state} Or its not a state in the given list" unless CITIES.has_key?(state)
     CITIES[state]
+  end
+
+  def is_invalid_key?(objects, value)
+    is_valid = objects.select{|object| object if [object[:name].downcase, object[:code].downcase].include?(value)}
+    is_valid.empty?
   end
 end
--- spec/world_wrap_spec.rb
+++ spec/world_wrap_spec.rb
@@ -26,6 +26,11 @@ describe WorldWrap do
       actual = states({country: 'INDIA'})
       expected.should == actual
     end
+    it 'should return TODO message' do
+      expected = "Need to save data for af"
+      actual = states({country: 'AF'})
+      expected.should == actual
+    end
   end
 
   describe '#cities' do
@@ -38,6 +43,19 @@ describe WorldWrap do
     it 'should return city when state is found' do
       expected = CITIES['ap']
       actual = cities({state: 'AP'})
+      expected.should == actual
+    end
+  end
+
+  describe '#is_valid_key?' do
+    it 'should return false when code or name not matched' do
+      expected = true
+      actual = is_invalid_key?(COUNTRIES, 'dilkhush')
+      expected.should == actual
+    end
+    it 'should return true when code or name matched' do
+      expected = false
+      actual = is_invalid_key?(COUNTRIES, 'in')
       expected.should == actual
     end
   end
